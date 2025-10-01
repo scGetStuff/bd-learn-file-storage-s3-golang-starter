7111d6faa2e40e4350d58dd187c5098fad2bfb05
scGetStuff <67354101+scGetStuff@users.noreply.github.com>
2025-10-01 15:49:17 -0400

    CH4 L3 broken for now, mostly done

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 	"io"
+	"math"
 	"mime"
 	"net/http"
 	"os"
+	"os/exec"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -81,6 +86,17 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	// TODO: CH4 L3
+	aspect, err := getVideoAspectRatio(tmp.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "getVideoAspectRatio() failed", err)
+		return
+	}
+	if true {
+		return
+	}
+	_ = aspect
+
 	assetPath := getAssetPath(mediaType)
 	_, err = cfg.s3Client.PutObject(context.Background(), &s3.PutObjectInput{
 		Bucket:      &cfg.s3Bucket,
@@ -102,4 +118,50 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	}
 
 	respondWithJSON(w, http.StatusOK, video)
+}
+
+func getVideoAspectRatio(filePath string) (string, error) {
+
+	// based off return from test run
+	var ffprobeOut struct {
+		Streams []struct {
+			Width  int `json:"width"`
+			Height int `json:"height"`
+		} `json:"streams"`
+	}
+
+	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filePath)
+	// fmt.Println(cmd.String())
+
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+
+	err := cmd.Run()
+	if err != nil {
+		return "Run() failed", err
+	}
+
+	if err = json.Unmarshal(stdout.Bytes(), &ffprobeOut); err != nil {
+		return "Unmarshal() failed", err
+	}
+
+	width := ffprobeOut.Streams[0].Width
+	height := ffprobeOut.Streams[0].Height
+	s := fmt.Sprintf("%v:%v", width, height)
+	fmt.Printf("Dimensions: %v\n", s)
+
+	ratio := float64(width) / float64(height)
+	ratio *= 100
+	ratio = math.Trunc(ratio)
+	fmt.Printf("Ratio: %v\n", ratio)
+
+	out := "other"
+	if int64(ratio) == 177 {
+		out = "16:9"
+	}
+	if int64(ratio) == 56 {
+		out = "9:16"
+	}
+
+	return out, nil
 }
